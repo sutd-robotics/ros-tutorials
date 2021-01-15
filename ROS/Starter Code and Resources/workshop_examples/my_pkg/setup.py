8ae4f521770ec5e41f9396e14076333e0703c995
Kevinskwk <kevinskwk@gmail.com>
2021-01-15 21:38:14 +0800

    upload activity package

---
+++ ROS/Starter Code and Resources/workshop_examples/my_pkg/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+setup_args = generate_distutils_setup(
+    packages = ['my_pkg'],
+    package_dir = {'': 'src'},
+    install_requires = ['']
+)
+
+setup(**setup_args)
+
